e71dd133f7d75cd4b95cdbc90c8277e9ccc533ce
Aayush Kumar <99812709+Aayush-Kr-13@users.noreply.github.com>
2023-10-13 01:15:59 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Aayush Kumar",
+    username: "https://github.com/Aayush-Kr-13",
+  },
 ];
